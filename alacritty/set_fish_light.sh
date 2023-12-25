650823b322947d1af08cab87395de7dc14fd51be
fovt <chenzigui1762@phytium.com.cn>
2023-12-25 09:43:19 +0800

    add alacritty config

---
+++ alacritty/set_fish_light.sh
@@ -0,0 +1,38 @@
+#!/bin/fish
+set -U fish_color_normal normal
+set -U fish_color_command 000087
+set -U fish_color_quote 024A68
+set -U fish_color_redirection 63AFD0
+set -U fish_color_end 8D003B
+set -U fish_color_error EC3B86
+set -U fish_color_param 225E79
+set -U fish_color_comment FFE100
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 3BA3D0
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_background
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=brblack
+set -U fish_pager_color_selected_prefix
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_selected_description
+set -U fish_color_host_remote
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_secondary_prefix
+set -U fish_color_keyword
+set -U fish_pager_color_secondary_description
+set -U fish_color_option
+set -U fish_pager_color_secondary_completion
