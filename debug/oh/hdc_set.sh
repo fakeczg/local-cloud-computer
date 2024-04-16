5d438f2553c7d92699555a2f797331cd81297354
fovt <chenzigui1762@phytium.com.cn>
2024-04-16 16:27:57 +0800

    add info oh-debug,update hdc ip and rc.conf ranger: gd -> oh-debug

--- debug/oh/hdc_set.sh
+++ debug/oh/hdc_set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ifconfig eth0 10.31.93.178 netmask 255.255.255.0
-route add default gw 10.31.93.254
+ifconfig eth0 10.31.90.121 netmask 255.255.252.0
+route add default gw 10.31.90.254
 param set persist.hdc.mode tcp
 param set persist.hdc.port 9527
 hdcd &
